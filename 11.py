e7da0f454e1957abc264e78583f0fd5ad47f931b
IconPriagamis <iconpriagamis0505@gmail.com>
2025-05-24 13:11:29 +0700

    Update 11.py

--- 11.py
+++ 11.py
@@ -16,7 +16,7 @@ if page == "Beranda":
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("curah hujan papua.csv")
+    df = pd.read_csv("curah hujan papua barat.csv")
     df.columns = df.columns.str.strip()
     return df
 
